01514e6764e9c493b6dbeb6cddb14a329ddb236e
Remko Duursma <remkoduursma@gmail.com>
2017-08-11 22:07:05 +1000

    Using multiple tabs; added WSU logo and changed colour to proper colour

--- app.R
+++ app.R
@@ -39,32 +39,71 @@ server <- function(input, output, session) {
 }
 
 
+  
 header <- dashboardHeader(
-  title = "Woodstock project"
-)
-
-body <- dashboardBody(
-  fluidRow(
-    column(width = 5,
-           box(width = 300, solidHeader = TRUE,
-               leafletOutput("mymap", height = 500)
-           )
-    ),
-    column(width=7,
-           fluidRow(infoBox("# Nurseries", nrow(locations), icon=icon("info-circle"))),
-           fluidRow(infoBox("# Batches", nrow(treestats), icon=icon("group"))),
-           fluidRow(infoBox("# Species", length(unique(treestats$species)), icon=icon("tree")))
-    )
-  ),
-  fluidRow(
-    box(DT::dataTableOutput("treestatsdata"))
+  title = tags$a(href='https://www.westernsydney.edu.au/hie',
+                 tags$img(src='wsu_badge_invert_small.png'),
+                  tags$style(HTML('.skin-black .main-header .logo {
+                              background-color: #9F2137;
+                              }
+                              .skin-black .main-header .logo:hover {
+                              background-color: #9F2137;
+                              }'))
   )
 )
 
-ui <- dashboardPage(
-  header,
-  dashboardSidebar(),
-  body
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+            h2("Dashboard tab content")
+    ),
+    
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
+    )
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "map",
+      fluidRow(
+        column(width = 5,
+               box(width = 300, solidHeader = TRUE,
+                   title = "Locations of nurseries included in the study",
+                   footer= "Zoom with +/-, move the map by dragging",
+                   leafletOutput("mymap", height = 500)
+               )
+        ),
+        column(width=7,
+               fluidRow(box(p("Some text goes here describing what is to be found here. Like iusb siub ciubviub coish siufsv ojsci cdois  codi scoin."))),
+               fluidRow(infoBox("# Nurseries", nrow(locations), icon=icon("info-circle"))),
+               fluidRow(infoBox("# Batches", nrow(treestats), icon=icon("group"))),
+               fluidRow(infoBox("# Species", length(unique(treestats$species)), icon=icon("tree")))
+        )
+      )),
+    tabItem(tabName ="data",
+      fluidRow(
+        box(solidHeader=TRUE,
+            title="Information on all batches sampled.",
+            DT::dataTableOutput("treestatsdata"))
+      )
+    )
+  )
+)
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Map", tabName = "map", icon = icon("map-o")),
+    menuItem("Data", icon = icon("database"), tabName = "data")
+  )
+)
+
+ui <- dashboardPage(  header,
+  sidebar,
+  body,
+  skin="black"
 )
 
 shinyApp(ui, server)
